0dfeb08b7aa08d00b5135fbf058c9aed0ba20b47
Romain Morel <morelromain86@gmail.com>
2021-06-22 15:51:01 +0200

    add and manage app profiles

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from lettings import views as views_l
+from profiles import views as views_p
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('', views_l.index, name='index'),
+    path('lettings/', views_l.lettings_index, name='lettings_index'),
+    path('lettings/<int:letting_id>/', views_l.letting, name='letting'),
+    path('profiles/', views_p.profiles_index, name='profiles_index'),
+    path('profiles/<str:username>/', views_p.profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
