5b3178d2d119b2a7a1ac3a08642625e9830f600f
Romain Morel <morelromain86@gmail.com>
2021-07-03 14:56:26 +0200

    add setting local/production management

---
+++ oc_lettings_site/settings/local.py
@@ -0,0 +1,9 @@
+from .base import *
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+DEBUG = True
+SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+ALLOWED_HOSTS = ['127.0.0.1', "0.0.0.0"]
---
+++ oc_lettings_site/settings/production.py
@@ -0,0 +1,25 @@
+from .base import *
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+DEBUG = False
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+ALLOWED_HOSTS = ['django-lettings.herokuapp.com']
+
+# Sentry configuration
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_NAME"),
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
