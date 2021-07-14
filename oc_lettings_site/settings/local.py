6756c0912ae1d4fb975e6488f03ed47f6a4843ad
Romain Morel <morelromain86@gmail.com>
2021-07-14 17:21:26 +0200

    Django setting module change

--- oc_lettings_site/settings/local.py
+++ oc_lettings_site/settings/local.py
@@ -6,5 +6,5 @@ from .base import *
 
 DEBUG = True
 SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
-ALLOWED_HOSTS = ['127.0.0.1', "0.0.0.0", "testserver", ".herokuapp.com"]
+ALLOWED_HOSTS = ['127.0.0.1', "0.0.0.0", "testserver"]
 
