c0966a56a933d502e1466f3cce1a08ab94c77c2b
Romain Morel <morelromain86@gmail.com>
2021-07-14 14:18:13 +0200

    repare static root

--- oc_lettings_site/settings/local.py
+++ oc_lettings_site/settings/local.py
@@ -8,8 +8,3 @@ DEBUG = True
 SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 ALLOWED_HOSTS = ['127.0.0.1', "0.0.0.0", "testserver"]
 
-STATIC_ROOT = ''
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
-]
